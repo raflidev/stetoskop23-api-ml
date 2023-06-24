9bf30f08be1d10eb2082cdac536bc46ac28a0b17
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-06-25 02:47:24 +0700

    first commit

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+import script as model
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<p>Hello, World!</p>"
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def cek():
+  if request.method == 'POST':
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    return model.predict(f.filename)
+
+if __name__ == "__name__":
+  app.run(debug=True)
